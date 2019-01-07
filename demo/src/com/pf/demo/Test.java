b8cf65fd875f92e2a1d2d4c5811599e1b8ed7b0c
topanfei <topanfei@163.com>
2019-01-07 16:43:35 +0800

    首次提交

---
+++ demo/src/com/pf/demo/Test.java
@@ -0,0 +1,12 @@
+package com.pf.demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Test.mainBranch();
+	}
+	
+	public static void mainBranch() {
+		System.out.println("主分支添加...");
+	}
+}
