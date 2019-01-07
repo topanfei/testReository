a150cdbe77d270f2e77e601f8885ff95b579fb11
topanfei <topanfei@163.com>
2019-01-07 17:13:27 +0800

    分支1提交

---
+++ demo/src/com/pf/demo/Branch1Test.java
@@ -0,0 +1,8 @@
+package com.pf.demo;
+
+public class Branch1Test {
+
+	public void testBranch1() {
+		System.out.println("分支1上添加的方法");
+	}
+}
--- demo/src/com/pf/demo/Test.java
+++ demo/src/com/pf/demo/Test.java
@@ -4,6 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		Test.mainBranch();
+		Branch1Test b1 = new Branch1Test();
+		b1.testBranch1();
 	}
 	
 	public static void mainBranch() {
